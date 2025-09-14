8e62e6ecc8cd3971c65fa31209096bce1af0c21c
Oleg Ognevoi <Ognevoi@hotmail.com>
2025-09-14 21:15:33 +0300

    fix type and move Minio configuration to user secrets

--- PetFamily.Backend/src/PetFamily.API/Program.cs
+++ PetFamily.Backend/src/PetFamily.API/Program.cs
@@ -1,17 +1,20 @@
+using Serilog;
+using Serilog.Events;
+
 using PetFamily.API;
 using PetFamily.API.Extensions;
 using PetFamily.API.Middlewares;
 using PetFamily.Application;
 using PetFamily.Infrastructure;
-using Serilog;
-using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// --- Logging ---
 Log.Logger = new LoggerConfiguration()
     .ReadFrom.Configuration(builder.Configuration)
     .WriteTo.Console()
-    .WriteTo.Seq(builder.Configuration.GetConnectionString("Seq") ?? throw new ArgumentNullException("Seq"))
+    .WriteTo.Seq(builder.Configuration.GetConnectionString("Seq") 
+                 ?? throw new ArgumentNullException("Seq"))
     .Enrich.WithThreadId()
     .Enrich.WithEnvironmentName()
     .MinimumLevel.Override("Microsoft.AspNetCore.Hosting", LogEventLevel.Warning)
@@ -19,6 +22,7 @@ Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Override("Microsoft.AspNetCore.Routing", LogEventLevel.Warning)
     .CreateLogger();
 
+// --- Services ---
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -32,27 +36,22 @@ builder.Services
 
 var app = builder.Build();
 
+// --- Middleware ---
 app.UseExceptionMiddleware();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    
+
     await app.ApplyMigrations();
 }
 
 app.UseSerilogRequestLogging();
-
 app.UseHttpsRedirection();
 
+// --- Endpoints ---
 app.MapControllers();
-
-app.Use(async (context, next) =>
-{
-    Console.WriteLine(context.Request.Path +  " start");
-    await next.Invoke();
-}); 
 
 app.Run();
 
@@ -60,7 +59,7 @@ namespace PetFamily.API
 {
     public partial class Program
     {
-        // This partial class is used to allow the Program class to be extended in tests.
-        // It is necessary for the WebApplicationFactory to work correctly in integration tests.
+        // This partial class is used to allow the Program class 
+        // to be extended in tests (for WebApplicationFactory).
     }
 }
